168ab871b8d58329a6ddcc1d1303e6b92623f20e
Maotou <93722462+maotou-spy@users.noreply.github.com>
2024-10-24 20:13:35 +0700

    [maotou][add PagingEnum.cs]

---
+++ SmE-CommerceModels/Enums/PagingEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace SmE_CommerceModels.Enums;
+
+public class PagingEnum
+{
+    public const int mobilePageSize = 10;
+    public const int PageSize = 20;
+    public const int PageNumber = 1;
+}
