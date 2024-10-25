06d513a9d755bb3532c30ee91e396670a08d4fb5
Ducbh <117875129+3mallrice@users.noreply.github.com>
2024-10-26 00:45:31 +0700

    [3mallrice][commit]

---
+++ SmE-CommerceServices/Interface/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using SmE_CommerceModels.Models;
+using SmE_CommerceModels.ReturnResult;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmE_CommerceServices.Interface
+{
+    public interface IProductService
+    {
+        Task<Return<Product>> AddProductAsync(Product product);
+    }
+}
