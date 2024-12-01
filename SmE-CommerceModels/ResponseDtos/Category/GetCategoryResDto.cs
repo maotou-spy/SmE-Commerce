7ea161f3de681def11807c2a71ced1739a24545f
Maotou <93722462+maotou-spy@users.noreply.github.com>
2024-12-02 00:42:44 +0700

    [maotou][update source code]

--- SmE-CommerceModels/ResponseDtos/Category/GetCategoryResDto.cs
+++ SmE-CommerceModels/ResponseDtos/Category/GetCategoryResDto.cs
@@ -1,12 +1,8 @@
 ﻿namespace SmE_CommerceModels.ResponseDtos.Category;
 
-public class GetCategoryDetailResDto
+public class GetCategoryResDto
 {
     public required Guid CategoryId { get; set; }
 
     public required string CategoryName { get; set; }
-
-    public string? Description { get; set; }
-
-    public string? CategoryImage { get; set; }
 }
